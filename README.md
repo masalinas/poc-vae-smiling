a34aa16800f0ceba83832f491633a053d6c364ee
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2026-01-12 18:45:26 +0100

    last commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Description
+VAE Smiling Model to be train in a Mac M1 or later using the [FFHQ dataset](https://github.com/NVlabs/ffhq-dataset)
+
+# Environment
+Create environment space. Right now only exist tensorflow for M1, etc architecture GPU up to the version Python 3.11 included:
+```
+python3.10 -m venv .venv
+source .venv/bin/activate
+```
+
+# Dependencies
+```
+pip install --upgrade pip setuptools wheel
+pip install --upgrade tensorflow-macos tensorflow-metal
+```
+
+# Dataset
+To train our VAE we will use the FFHQ Smiling Faces Dataset from this link. Get the thumbnails128x128.zip (1.97 GB) from ffhq-dataset folder to training your model
+
+```
+https://drive.google.com/drive/folders/1u2xu7bSrWxrbUxk-dT-UvEJq8IjdmNTP
+```
---
+++ main.py
@@ -0,0 +1,195 @@
+import os
+import tensorflow as tf
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+
+DATA_DIR = "dataset/ffhq/thumbnails_128x128"
+
+IMG_SIZE = 128
+BATCH_SIZE = 64
+EPOCHS = 50
+
+BETA = 4.0  # β-VAE for disentanglement
+
+latent_dim = 16
+
+class VAE(tf.keras.Model):
+    def __init__(self, encoder, decoder, beta=1.0, **kwargs):
+        super().__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+        self.beta = beta
+
+        self.total_loss_tracker = tf.keras.metrics.Mean(name="total_loss")
+        self.recon_loss_tracker = tf.keras.metrics.Mean(name="recon_loss")
+        self.kl_loss_tracker = tf.keras.metrics.Mean(name="kl_loss")
+    
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.recon_loss_tracker,
+            self.kl_loss_tracker,
+        ]
+
+    def train_step(self, x):
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var, z = self.encoder(x)
+            x_recon = self.decoder(z)
+
+            # Reconstruction loss (pixel-wise)
+            recon_loss = tf.reduce_mean(
+                tf.reduce_sum(
+                    tf.keras.losses.binary_crossentropy(x, x_recon),
+                    axis=(1, 2)
+                )
+            )
+
+            # KL divergence
+            kl_loss = -0.5 * tf.reduce_mean(
+                tf.reduce_sum(
+                    1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var),
+                    axis=1
+                )
+            )
+
+            total_loss = recon_loss + self.beta * kl_loss
+
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+
+        self.total_loss_tracker.update_state(total_loss)
+        self.recon_loss_tracker.update_state(recon_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "recon_loss": self.recon_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        }
+
+    def generate(self, n_samples=1):
+        # Sample from standard normal latent space
+        z = tf.random.normal(shape=(n_samples, self.encoder.output[0].shape[-1]))
+        generated = self.decoder(z)
+
+        return generated
+    
+def load_image(path):
+    img = tf.io.read_file(path)
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.resize(img, (IMG_SIZE, IMG_SIZE))
+    img = tf.cast(img, tf.float32) / 255.0
+
+    return img
+
+def sampling(args):
+    z_mean, z_log_var = args
+    eps = tf.random.normal(shape=tf.shape(z_mean))
+
+    return z_mean + tf.exp(0.5 * z_log_var) * eps
+
+def show_reconstructions(model, dataset, n=8):
+    x = next(iter(dataset))
+    z_mean, _, _ = model.encoder(x)
+    x_recon = model.decoder(z_mean)
+
+    plt.figure(figsize=(16, 4))
+    for i in range(n):
+        # Original
+        plt.subplot(2, n, i + 1)
+        plt.imshow(x[i])
+        plt.axis("off")
+
+        # Reconstruction
+        plt.subplot(2, n, i + n + 1)
+        plt.imshow(x_recon[i])
+        plt.axis("off")
+
+    plt.savefig("results/show_reconstructions.png")
+
+def sample_faces(decoder, n=16):
+    z = tf.random.normal(shape=(n, latent_dim))
+    imgs = decoder(z)
+
+    plt.figure(figsize=(6, 6))
+    for i in range(n):
+        plt.subplot(4, 4, i + 1)
+        plt.imshow(imgs[i])
+        plt.axis("off")
+        
+    plt.savefig("results/sample_faces.png")
+
+def load_model(encoder_name, decoder_name):
+    encoder = load_model(encoder_name, compile=False)
+    decoder = load_model(decoder_name, compile=False)
+
+    return encoder, decoder
+
+print("🟢 check GPU") 
+print(tf.__version__)
+print(tf.config.list_physical_devices('GPU'))
+
+print("🟢 Load dataset") 
+image_paths = tf.data.Dataset.list_files(
+    os.path.join(DATA_DIR, "*.png"),
+    shuffle=True
+)
+
+dataset = (
+    image_paths
+    .map(load_image, num_parallel_calls=tf.data.AUTOTUNE)
+    .shuffle(10000)
+    .batch(BATCH_SIZE)
+    .prefetch(tf.data.AUTOTUNE)
+)
+
+print("🟢 Encoder") 
+encoder_inputs = layers.Input(shape=(128, 128, 3))
+x = layers.Conv2D(32, 4, strides=2, padding="same", activation="relu")(encoder_inputs)   # 64×64
+x = layers.Conv2D(64, 4, strides=2, padding="same", activation="relu")(x)                # 32×32
+x = layers.Conv2D(128, 4, strides=2, padding="same", activation="relu")(x)               # 16×16
+x = layers.Conv2D(256, 4, strides=2, padding="same", activation="relu")(x)               # 8×8
+
+x = layers.Flatten()(x)
+x = layers.Dense(512, activation="relu")(x)
+
+z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
+
+z = layers.Lambda(sampling, name="z")([z_mean, z_log_var])
+
+encoder = tf.keras.Model(encoder_inputs, [z_mean, z_log_var, z])
+
+print("🟢 Decoder") 
+latent_inputs = layers.Input(shape=(latent_dim,))
+
+x = layers.Dense(8 * 8 * 256, activation="relu")(latent_inputs)
+x = layers.Reshape((8, 8, 256))(x)
+
+x = layers.Conv2DTranspose(256, 4, strides=2, padding="same", activation="relu")(x)  # 16×16
+x = layers.Conv2DTranspose(128, 4, strides=2, padding="same", activation="relu")(x)  # 32×32
+x = layers.Conv2DTranspose(64, 4, strides=2, padding="same", activation="relu")(x)   # 64×64
+x = layers.Conv2DTranspose(32, 4, strides=2, padding="same", activation="relu")(x)   # 128×128
+
+decoder_outputs = layers.Conv2D(3, 3, padding="same", activation="sigmoid")(x)
+
+decoder = tf.keras.Model(latent_inputs, decoder_outputs)
+
+print("🟢 VAE model") 
+vae = VAE(encoder, decoder, beta=BETA)
+
+vae.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4))
+
+print("🟢 Train model") 
+vae.fit(dataset, epochs=EPOCHS)
+
+print("🟢 Sample new faces from the latent space")
+sample_faces(decoder)
+
+print("🟢 Visualize reconstructions model") 
+show_reconstructions(vae, dataset)
+
+print("🟢 Save model")
+encoder.save("models/encoder.keras")
+decoder.save("models/decoder.keras")
